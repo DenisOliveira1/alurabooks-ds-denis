4e97dba3234773d30a4258db9920bce4963811f4
Denis Oliveira <doliveira@vidalink.com.br>
2024-05-13 19:13:15 -0300

    Finalizando modulo 2

---
+++ alurabooks-consumer/src/App.tsx
@@ -0,0 +1,12 @@
+import { AbBotao } from 'ds-alurabooks-base-denis';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <AbBotao></AbBotao>
+    </div>
+  );
+}
+
+export default App;
